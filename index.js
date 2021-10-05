66e22cddcf1ea0eb964f7b36b5ab2eaf54ae3cd8
akshit-jain01 <akshitjain.1112003@gmail.com>
2021-10-05 19:40:30 +0530

    added stop button and sounds

--- index.js
+++ index.js
@@ -2,9 +2,33 @@ function clicked()
 {
     var x = document.getElementsByClassName("sky");
     var i=0;
-        setInterval(() => {
+    k = setInterval(() => {
                     x[0].style.backgroundPositionX=i+"px";
                     i++;
-        }, 0.9);    
-        
+        }, 1); 
+        audio.play();
 }
+function clicked2(){
+    clearInterval(k);
+    audio.pause();
+
+} 
+ 
+
+var audio = document.createElement('audio');
+audio.setAttribute('src','jet-sound-effect.mp3');
+audio.loop=false;
+
+// let arr=['url(images/bird1.png)','url(images/bird2.png)','url(images/bird3.png)','url(images/bird4.png)'];
+// setInterval(anim(),20);
+// function anim()
+// {
+//     let a=0;
+//     var obj = document.getElementById("bird");
+    
+//     a++;
+//     if(a==4)
+//     a=0;
+//     obj.style.backgroundImage = arr[a];
+
+// }
